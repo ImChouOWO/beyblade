072c83d7c03c5991de55a80d13c574b7f59fbddb
Chou_isme <54586385+ImChouOWO@users.noreply.github.com>
2026-06-06 14:40:47 +0800

    Initial Commit

---
+++ beyblade/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  beyblade
+//
+//  Created by 周呈翰 on 2026/6/6.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ beyblade/beybladeApp.swift
@@ -0,0 +1,17 @@
+//
+//  beybladeApp.swift
+//  beyblade
+//
+//  Created by 周呈翰 on 2026/6/6.
+//
+
+import SwiftUI
+
+@main
+struct beybladeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
